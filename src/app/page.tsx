74ca9bbfd3dae0b3dc14cc58e0dc1355c2fda89a
Victor Hugo <victorhugobeppler@gmail.com>
2023-04-23 10:20:31 -0300

    Build(add style in footer and padding sidebar and main)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-64 bg-zinc-950">
+        <aside className="w-64 bg-zinc-950 p-6">
           Side bar
         </aside>
-        <main className="flex-1">
+        <main className="flex-1 p-6">
           main
         </main>
       </div>
-      <footer className="">
+      <footer className="bg-zinc-800 border-t  border-zinc-700 p-6">
         Footer
       </footer>
     </div>
