5378f2749584a844ccb62b7b790ddc76c1037bcd
Victor Hugo <victorhugobeppler@gmail.com>
2023-04-23 10:51:00 -0300

    Build(update code)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
+import { Home as Homeicon, Search, Library } from 'lucide-react'
+
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-64 bg-zinc-950 p-6">
-          Side bar
+        <aside className="w-72 bg-zinc-950 p-6">
+          <div className='flex items-center gap-2'>
+            <div className='w-3 h-3 bg-red-500 rounded-full' />
+            <div className='w-3 h-3 bg-yellow-500 rounded-full' />
+            <div className='w-3 h-3 bg-green-500 rounded-full' />
+          </div>
+          <nav className='space-y-5 mt-10'>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Homeicon />
+              Home
+            </a>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Search />
+              Search
+            </a>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Library />
+              Your Library
+            </a>
+          </nav>
+
+          <nav className='mt-6 pt-10 border-t border-zinc-800 flex flex-col gap-2'>
+            <a className='text-sm text-zinc-400' href=''>Playlist 1</a>
+            <a className='text-sm text-zinc-400' href=''>Playlist 2</a>
+            <a className='text-sm text-zinc-400' href=''>Playlist 3</a>
+          </nav>
         </aside>
         <main className="flex-1 p-6">
           main
