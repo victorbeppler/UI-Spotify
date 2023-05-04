5bc57f58a9e45b9036f2c0ea1f2b3684da3b9d88
Victor Hugo <victorhugobeppler@gmail.com>
2023-05-04 00:43:42 -0300

    build(Create page Liked Songs):
    Add new components in page:
    - Header page
    - Buttons -> Play, Pause, Search and Filter Date
    - Create and add Playlist table in main

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,9 +1,10 @@
-import { ChevronLeft, ChevronRight, ChevronDown } from "lucide-react";
+import { ChevronLeft, ChevronRight, ChevronDown, Search } from "lucide-react";
 import React from "react";
+import { data } from "../../Services/Playlists.js";
 
 export default function Main() {
   return (
-    <main className="flex-1">
+    <main className="flex-1 h-full bg-zinc-900">
       <div className="bg-gradient-to-b  from-purple-700 to-purple-900-600 max-h-96">
         <div className="flex justify-between p-6">
           <div className="flex items-center gap-4 start-0">
@@ -15,7 +16,7 @@ export default function Main() {
             </button>
           </div>
           <div className="flex gap-2 border h-8 border-gray-50 rounded-full pr-1 pl-1 hover:translate-y-0.5">
-            <img src="./CapaAlanWalker.jpeg" className="w-6 rounded-full" />
+            <img src="./victor.jpeg" className="w-8 h-7 rounded-full" />
             victorhb
             <div className="flex align-bottom pt-1">
               <ChevronDown size={20} />
@@ -23,208 +24,95 @@ export default function Main() {
           </div>
         </div>
         <div className="flex flex-row h-64">
-          <div className="w-44 h-44 bg-zinc-200 mr-4 ml-6"></div>
+          <div className="border w-44 h-44 bg-gradient-to-br  from-darkPurple to-purple-900 shadow-sm shadow-black mr-4 ml-6 flex items-center justify-center">
+            <img src="/heart-fill-96px-white.png" alt="Heart icon" />
+          </div>
           <div className="flex flex-col pt-6">
             <div className="font-normal text-sm">Playlist</div>
-            <div className="font-semibold text-7xl">Liked Musics</div>
-            <div className="font-normal text-sm flex flex-row gap-1 pt-5">
-              <img src="./CapaAlanWalker.jpeg" className="w-6 rounded-full" />
+            <div className="font-semibold text-7xl">Liked Songs</div>
+            <div className="font-normal text-sm flex flex-row gap-1 pt-5 items-center">
+              <img src="./victor.jpeg" className="w-6 h-6 rounded-full" />
               victorhb . 257 musics
             </div>
           </div>
         </div>
       </div>
-      <div className="bg-gradient-to-t  from-purple-950 to-zinc-900 h-32">
-        <div className="flex w-12 h-12 bg-greenNeon rounded-full justify-center items-center ml-8">
+      <div className="bg-gradient-to-t  from-purple-950 to-zinc-900 h-32 flex flex-row gap-6">
+        <div className="flex w-14 h-14 bg-greenNeon rounded-full justify-center items-center ml-8">
           <img
             src="/play-fill.svg"
-            width={18}
-            height={18}
+            width={24}
+            height={24}
             alt="Play button in playlist"
           />
         </div>
+        <div className="pt-3">
+          <img src="./pause-circle.png" />
+        </div>
+        <div className="flex absolute right-0">
+          <div className="mr-12 mt-5 flex items-center">
+            <Search />
+          </div>
+          <div className="flex mr-12 mt-5 gap-1 items-center">
+            <a href="#" className="text-zinc-50 font-extralight text-sm">
+              Date added
+            </a>
+            <ChevronDown />
+          </div>
+        </div>
       </div>
-      {/* <h2 className='text-xl font-semibold'>Your top genres of 2021</h2>
-      <p className='text-zinc-400 mt-2'>Your top genres of 2021</p> */}
-      {/* <h1 className="font-semi-bold text-3xl mt-10">Good Afternoon</h1>
-            <div className="grid grid-cols-3 gap-4 mt-4 ">
-                <a
-                    href="#"
-                    className="bg-white/5 rounded group overflow-hidden gap-4 flex items-center hover:bg-white/10 transition-colors"
-                >
-                    <img src="/album.jpg" alt="" width={104} height={104}></img>
-                    <strong>Playlist</strong>
-                    <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
-                        <img
-                            src="/play-fill.svg"
-                            width={20}
-                            height={20}
-                            alt="Play button in playlist"
-                        />
-                    </button>
-                </a>
-                <a
-                    href="#"
-                    className="bg-white/5 rounded group overflow-hidden gap-4 flex items-center hover:bg-white/10 transition-colors"
-                >
-                    <img src="/album.jpg" alt="" width={104} height={104}></img>
-                    <strong>Playlist</strong>
-                    <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
-                        <img
-                            src="/play-fill.svg"
-                            width={20}
-                            height={20}
-                            alt="Play button in playlist"
-                        />
-                    </button>
-                </a>
-                <a
-                    href="#"
-                    className="bg-white/5 rounded group overflow-hidden gap-4 flex items-center hover:bg-white/10 transition-colors"
-                >
-                    <img src="/album.jpg" alt="" width={104} height={104}></img>
-                    <strong>Playlist</strong>
-                    <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
-                        <img
-                            src="/play-fill.svg"
-                            width={20}
-                            height={20}
-                            alt="Play button in playlist"
-                        />
-                    </button>
-                </a>
-                <a
-                    href="#"
-                    className="bg-white/5 rounded group overflow-hidden gap-4 flex items-center hover:bg-white/10 transition-colors"
-                >
-                    <img src="/album.jpg" alt="" width={104} height={104}></img>
-                    <strong>Playlist</strong>
-                    <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
-                        <img
-                            src="/play-fill.svg"
-                            width={20}
-                            height={20}
-                            alt="Play button in playlist"
-                        />
-                    </button>
-                </a>
-                <a
-                    href="#"
-                    className="bg-white/5 rounded group overflow-hidden gap-4 flex items-center hover:bg-white/10 transition-colors"
-                >
-                    <img src="/album.jpg" alt="" width={104} height={104}></img>
-                    <strong>Playlist</strong>
-                    <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
-                        <img
-                            src="/play-fill.svg"
-                            width={20}
-                            height={20}
-                            alt="Play button in playlist"
-                        />
-                    </button>
-                </a>
-                <a
-                    href="#"
-                    className="bg-white/5 rounded group overflow-hidden gap-4 flex items-center hover:bg-white/10 transition-colors"
-                >
-                    <img src="/album.jpg" alt="" width={104} height={104}></img>
-                    <strong>Playlist</strong>
-                    <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
-                        <img
-                            src="/play-fill.svg"
-                            width={20}
-                            height={20}
-                            alt="Play button in playlist"
-                        />
-                    </button>
-                </a>
-            </div>
-
-            <h2 className="font-semi-bold text-2xl mt-10">
-                Made for Victor Hugo
-            </h2>
-            <div className="grid grid-cols-8 gap-4 mt-4">
-                <a
-                    href=""
-                    className="bg-white/5 p-2 rounded-lg flex flex-col gap-2 hover:bg-white/10"
-                >
+      <div className="bg-gradient-to-t  from-zinc-900 to-purple-950 h-full">
+        <table className="w-full text-left border-collapse shadow-md">
+          <thead className="text-white border-b-white">
+            <tr className="border-b-1 border-zinc-400">
+              <th className="py-4 px-6">#</th>
+              <th className="py-4 px-6">Title</th>
+              <th className="py-4 px-6">Album</th>
+              <th className="py-4 px-6 flex items-center gap-1">
+                Date added{" "}
+                <img
+                  src="./caret-down-fill.png"
+                  alt="Arrow Down Green"
+                  className="w-5 h-5"
+                />
+              </th>
+              <th className="py-4 px-6">Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => {
+              return (
+                <tr className="hover:border-1">
+                  <td className="py-4 px-6">{item.id}</td>
+                  <td className="py-4 px-6 flex gap-2">
                     <img
-                        src="/album.jpg"
-                        className="w-full"
-                        alt=""
-                        width={104}
-                        height={104}
-                    ></img>
-                    <strong className="font-semibold">Dailay Mix 1</strong>
-                    <span className="text-sm text-zinc-500">
-                        Wallows, COIN, girl in red and more
-                    </span>
-                </a>
-                <a
-                    href=""
-                    className="bg-white/5 p-2 rounded-lg flex flex-col gap-2 hover:bg-white/10"
-                >
-                    <img
-                        src="/album.jpg"
-                        className="w-full"
-                        alt=""
-                        width={104}
-                        height={104}
-                    ></img>
-                    <strong className="font-semibold">Dailay Mix 1</strong>
-                    <span className="text-sm text-zinc-500">
-                        Wallows, COIN, girl in red and more
-                    </span>
-                </a>
-                <a
-                    href=""
-                    className="bg-white/5 p-2 rounded-lg flex flex-col gap-2 hover:bg-white/10"
-                >
-                    <img
-                        src="/album.jpg"
-                        className="w-full"
-                        alt=""
-                        width={104}
-                        height={104}
-                    ></img>
-                    <strong className="font-semibold">Dailay Mix 1</strong>
-                    <span className="text-sm text-zinc-500">
-                        Wallows, COIN, girl in red and more
-                    </span>
-                </a>
-                <a
-                    href=""
-                    className="bg-white/5 p-2 rounded-lg flex flex-col gap-2 hover:bg-white/10"
-                >
-                    <img
-                        src="/album.jpg"
-                        className="w-full"
-                        alt=""
-                        width={104}
-                        height={104}
-                    ></img>
-                    <strong className="font-semibold">Dailay Mix 1</strong>
-                    <span className="text-sm text-zinc-500">
-                        Wallows, COIN, girl in red and more
-                    </span>
-                </a>
-                <a
-                    href=""
-                    className="bg-white/5 p-2 rounded-lg flex flex-col gap-2 hover:bg-white/10"
-                >
-                    <img
-                        src="/album.jpg"
-                        className="w-full"
-                        alt=""
-                        width={104}
-                        height={104}
-                    ></img>
-                    <strong className="font-semibold">Dailay Mix 1</strong>
-                    <span className="text-sm text-zinc-500">
-                        Wallows, COIN, girl in red and more
-                    </span>
-                </a>
-            </div> */}
+                      src={`data:image/png;base64,${item.image}`}
+                      alt="Capa Album"
+                      className="w-12 h-12"
+                    />
+                    <div>
+                      <div>{item.title}</div>
+                      <div>{item.author}</div>
+                    </div>
+                  </td>
+                  <td className="py-4 px-6">{item.album}</td>
+                  <td className="py-4 px-6">{item.addedIn}</td>
+                  <td className="py-4 px-6 gap-3">
+                    <div className="flex">
+                      <img
+                        src="./heart-fill-green.png"
+                        alt="Liked Icon"
+                        className="w-6 h-6"
+                      />
+                      {item.duration}
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 }
