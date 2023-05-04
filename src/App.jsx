2cf8b1a1fd76e51beca7cc7b4b47a3966180b1fc
Victor Hugo <victorhugobeppler@gmail.com>
2023-05-04 01:27:12 -0300

    fix(Resolve error in background):

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="bg-zinc-900 text-zinc-50 h-screen flex flex-col">
+    <div className="bg-zinc-900 text-zinc-50 h-full flex flex-col">
       <div className="flex flex-1">
         <Sidebar />
         <Main />
       </div>
-      <Footer />
+      <Footer className="" />
     </div>
   );
 }
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Sidebar() {
   return (
-    <aside className="w-72 bg-zinc-950 p-6">
+    <aside className="w-72 h-full p-6">
       <div className="flex items-center gap-2">
         <div className="w-3 h-3 bg-red-500 rounded-full" />
         <div className="w-3 h-3 bg-yellow-500 rounded-full" />
@@ -68,7 +68,7 @@ export default function Sidebar() {
         </a>
       </nav>
 
-      <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+      <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3 h-full">
         <a className="text-sm text-zinc-400 hover:text-zinc-100" href="">
           As mais tocadas no seu 2022
         </a>
